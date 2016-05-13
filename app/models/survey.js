6ce29fc806725b44d373aa0c89d05e72747ce1c5
philschoof <phil.schoof@gmail.com>
2016-05-13 15:16:27 -0400

    Create survey model

---
+++ app/models/survey.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const surveySchema = new mongoose.Schema({
+
+  question: {
+    type: String,
+    required: true,
+  },
+  options: {
+    required:true,
+    type: [mongoose.Schema.Types.Mixed],
+  },
+
+  _author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+}, {
+  timestamps: true,
+  toJSON: { virtuals: true },
+});
+
+// surveySchema.virtual('length').get(function length() {
+//   return this.text.length;
+// });
+
+const Survey = mongoose.model('Survey', surveySchema);
+
+module.exports = Survey;
