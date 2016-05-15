a00fa32ea209415b3aeaefc0c0de9a9d4f9eb913
soynog <nicolas.o.garcia@gmail.com>
2016-05-15 15:29:36 -0400

    Add Custom URL Virtual Property to Survey Model
    - Currently just a string to append to base URL
    - Should refactor later to return full url? Or else just create this on the front end.

--- app/models/survey.js
+++ app/models/survey.js
@@ -23,9 +23,10 @@ const surveySchema = new mongoose.Schema({
   toJSON: { virtuals: true },
 });
 
-// surveySchema.virtual('length').get(function length() {
-//   return this.text.length;
-// });
+// Virtual Property returns custom url for this Survey
+surveySchema.virtual('url').get(function url() {
+  return `?id=${this._id}`;
+});
 
 const Survey = mongoose.model('Survey', surveySchema);
 
